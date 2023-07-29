79b5ee7ca67da116b1233bdd34a91cb187486df8
Dipongkor-Roy <juniordipu@gmail.com>
2023-07-29 21:56:18 +0600

    signIn and SignOut emplemented

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './LogIn.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../Contexts/UserContext';
 const LogIn = () => {
+    const {signIn}=useContext(AuthContext)
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        signIn(email,password)
+        .then(result=>{
+            const user=result.user;
+            console.log(user);
+            form.reset();
+        })
+        .catch(error=>console.error(error));
+    }
     return (
         <div className='form-container'>
             <h3 className='form-title'>Log In</h3>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div className='form-control'>
-                    <label htmlFor="Email">Email</label>
-                    <input type="email" name="" required />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
                 </div>
                 <div className='form-control'>
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" required />
                 </div>
+                <input className='btn-submit' type="submit" value="LogIn" />
             </form>
+            <p className='text-down-btn'>New to Ema-John? <Link to="/signUp" className='new-acc'>Create New Account</Link></p>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './SignUp.css'
+import { AuthContext } from '../Contexts/UserContext';
 
 const SignUp = () => {
+    const [error,setError]=useState(null);
+    
+    const {createUser}=useContext(AuthContext);
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();//to prevent reload
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        const confirm=form.confirm.value;
+
+        if(password.length<8){
+            setError('Password Less than 8 characters');
+            return;
+        }
+        if(password !== confirm){
+            setError('Dont Match Password');
+            return;
+        }
+        createUser(email,password)
+        .then(result=>{
+            const user=result.user;
+            console.log(user);
+            form.reset();
+        })
+        .catch(error=>console.error(error))
+    }
     return (
-        <div>
-            <h3>SignUp</h3>
+        <div className='form-container'>
+            <h3 className='form-title'>Sign Up</h3>
+            <form onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor="Email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="password" name="confirm" required />
+                </div>
+                <input className='btn-submit' type="submit" value="SignUp" />
+            </form>
+            <p className='text-down-btn'>Already have an account? <Link to="/logIn" className='new-acc'>Log In</Link></p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
